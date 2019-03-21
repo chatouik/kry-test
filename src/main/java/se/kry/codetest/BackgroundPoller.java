45b6d0334f2a3acff8278ad9b28618f396de4f03
Chatoui Khalil <chatoui_khalil@hotmail.fr>
2019-03-21 02:12:06 +0100

    kry test

--- src/main/java/se/kry/codetest/BackgroundPoller.java
+++ src/main/java/se/kry/codetest/BackgroundPoller.java
@@ -10,8 +10,6 @@ import java.util.Random;
 public class BackgroundPoller {
 
   public Future<List<String>> pollServices(HashMap<String, Service> services) {
-    String a = "ree";
-    System.out.println("i am in poller");
     return Future.failedFuture("TODO");
   }
 }
